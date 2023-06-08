a84a6b0b81de579a0f30f7b5d1cf8ad7316f954c
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2023-06-08 21:38:21 +0200

    deploy contracts auto copy files

--- migrations/1_deploy_contract.js
+++ migrations/1_deploy_contract.js
@@ -10,8 +10,15 @@ module.exports = function (deployer) {
                 "mainSmartContractAddress": MainSmartContract.address
             }
         };
+        
+        if (!fs.existsSync("static/contracts/")){
+            fs.mkdirSync("static/contracts/");
+        }
 
         fs.writeFileSync("static/contracts/const.json", JSON.stringify(constants));
+        fs.copyFileSync("build/contracts/MainSmartContract.json", "static/contracts/MainSmartContract.json")
+        fs.copyFileSync("build/contracts/HNFT.json", "static/contracts/HNFT.json")
+
         console.log(MainSmartContract.address)
     })
 
